7f2e398678d78698880258b9d0d3b855a41885fd
noumanniazi <noumanaamir@nouman.home>
2020-02-05 22:32:59 +0500

    Updates

---
+++ precache-manifest.0553432a50216e2f0ecd561057e35b6f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e28180b70d747123888e061332267c24",
+    "url": "/new2arizona/index.html"
+  },
+  {
+    "revision": "4a4d3fdf893fc63e6427",
+    "url": "/new2arizona/static/css/main.e978b462.chunk.css"
+  },
+  {
+    "revision": "577c8ebac3e5e291d6fd",
+    "url": "/new2arizona/static/js/2.dd227c83.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/new2arizona/static/js/2.dd227c83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a4d3fdf893fc63e6427",
+    "url": "/new2arizona/static/js/main.3eaf72bf.chunk.js"
+  },
+  {
+    "revision": "1477a591b2335be56d71",
+    "url": "/new2arizona/static/js/runtime-main.22f7c1f7.js"
+  },
+  {
+    "revision": "2dee52b70a46490140141c7e6751444b",
+    "url": "/new2arizona/static/media/arizona.2dee52b7.jpg"
+  },
+  {
+    "revision": "65915c00c8d15759957fe08fbf951b0c",
+    "url": "/new2arizona/static/media/family.65915c00.png"
+  }
+]);
